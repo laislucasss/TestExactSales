3cd6aefe32fe8a192ca9eeb57f05762b827b89b5
Lais Ferreira <laisluccas@hotmail.com>
2021-08-18 23:39:19 -0300

    Estrutura do projeto

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteExactSales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+              string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=exactdev.database.windows.net;Initial Catalog=TesteTecnicoDEV;User ID=exactsales;Password=ug2nUyrEq40UYO4eWbRH";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "SELECT * FROM dbo.Pessoa RIGHT JOIN dbo.Cargo ON dbo.Pessoa.idCargo = dbo.Cargo.ID";
+
+            command = new SqlCommand(sql, cnn);
+
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(1) + "\n";
+            }
+            MessageBox.Show(Output);
+            cnn.Close();
+        }
+    }
+}
